c78a5a6df74ba4fdf30386ee2f24b7b3230c102e
Marlon <90068388+marlon-frazier@users.noreply.github.com>
2023-10-16 12:10:08 -0500

    finished game, future improvements: setting a score limit

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import turtle
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
 paddle = Turtle()
+scoreboard = Scoreboard()
 
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
@@ -27,7 +29,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -35,11 +37,19 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    #Detect collison with r_paddle
+    #Detect collison with paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    #Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
 
+    #Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
