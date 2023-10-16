47515d54bd0edbcd93432212e8875045df4d5b51
Marlon <90068388+marlon-frazier@users.noreply.github.com>
2023-10-16 11:27:11 -0500

    added Ball class

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import turtle
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 paddle = Turtle()
@@ -12,7 +14,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
+ball = Ball()
 
 
 screen.listen()
@@ -25,8 +27,9 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
-
+    ball.move()
 
 
 
